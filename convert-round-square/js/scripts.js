e0a255e77d6e9bcddc23a7dc2a27ce43413a1d06
Cesar Cobos <cesarcoboscv@gmail.com>
2022-06-28 17:50:27 -0500

    Commit inicial

---
+++ convert-round-square/js/scripts.js
@@ -0,0 +1,17 @@
+var x = document.getElementById("resultado");
+
+function roundsquare(a){
+   var area = Math.PI * (Math.pow(2,(a/2)));
+   return area
+   
+   
+}
+x.innerHTML = "Hlla";
+
+console.log(roundsquare(7))
+
+
+
+
+
+
