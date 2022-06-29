db099075a06f2e10aaf20b374e3ad33294381b43
Cesar Cobos <cesarcoboscv@gmail.com>
2022-06-29 12:16:28 -0500

    Terminado conversor de area, empezando conversor de unidades

--- convert-round-square/js/scripts.js
+++ convert-round-square/js/scripts.js
@@ -1,16 +1,34 @@
-// var  lad;
+function roundsquare() {
+   let user = document.getElementById("diametro");
+   let user2 = document.getElementById("lado");
+   let a =user.value;
+   let lado2 = user2.value;
 
-function roundsquare(a){
-   a = 15;
-   lado2 = 250;
    area = Math.PI * (Math.pow(((a *25.4)/2),2));
    lado1 = area /lado2;
-   return lado1;
+   lado1 = lado1.toFixed(2);
+   area = area.toFixed(2);
+   
+   document.getElementById("resultadoArea").innerText =  area + " ";
+   document.getElementById("resultado1").innerText =  lado1 + " ";
+   document.getElementById("resultado2").innerText =  lado2 + " ";
 }
 
-roundsquare();
+// roundsquare();
 
-// console.log(lado2);
-
-document.getElementById("resultado1").innerHTML = lado1;
-document.getElementById("resultado2").innerHTML = lado2;
+function converter(){
+   let user = document.getElementById("units");
+   let a = user.value;
+   if(a == "cmin"){
+      document.getElementById("userSelect").innerText = a;
+   }else if(a == "mmin"){
+      document.getElementById("userSelect").innerText = a;
+   }else if(a == "incm"){
+      document.getElementById("userSelect").innerText = a;
+   }else if(a == "inmm"){
+      document.getElementById("userSelect").innerText = a;
+   }
+   
+   
+}
+// converter();
