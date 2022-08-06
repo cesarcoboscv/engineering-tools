783b295b0f11a67e4e68d2397a709f7529b2d999
Cesar Cobos <cesarcoboscv@gmail.com>
2022-08-06 12:55:19 -0500

    Reducción de código

--- assets/js/batery.js
+++ assets/js/batery.js
@@ -1,45 +1,40 @@
-//nivel de barteria
+//Battery level
 navigator.getBattery().then(function(battery) {
-   function updateAllBatteryInfo(){
-     updateChargeInfo();
-     updateLevelInfo();
+	function updateAllBatteryInfo(){
+    	updateChargeInfo();
+    	updateLevelInfo();
     //  updateChargingInfo();
     //  updateDischargingInfo();
    }
-   updateAllBatteryInfo();
+	updateAllBatteryInfo();
  
+	battery.addEventListener('chargingchange', function(){
+    	updateChargeInfo();
+	});
 
-   battery.addEventListener('chargingchange', function(){
-     updateChargeInfo();
-   });
+	function updateChargeInfo(){
+        var element = document.getElementById("batstatus");
+        var a = Math.round(battery.level * 100);
+        a =a + "%";
+	// console.log("La batería esta cargando? " + (battery.charging ? "Si" : "No"));
+		if (battery.charging == true){
+		element.classList.remove("fa-battery-full");
+		element.classList.remove("fa-battery-three-quarters");
+		element.classList.remove("fa-battery-half");
+		element.classList.remove("fa-battery-quarter");
+		element.classList.remove("fa-battery-low");
+		element.classList.add("fa-bolt");
+		document.getElementById("bateryLevel").innerText = a; 
+		}
+    }
 
-   
-   function updateChargeInfo(){
-    var element = document.getElementById("batstatus");
-    var a = Math.round(battery.level * 100);
-      a =a + "%";
-     console.log("La batería esta cargando? " + (battery.charging ? "Si" : "No"));
-        if (battery.charging == true){
-          element.classList.remove("fa-battery-full");
-          element.classList.remove("fa-battery-three-quarters");
-          element.classList.remove("fa-battery-half");
-          element.classList.remove("fa-battery-quarter");
-          element.classList.remove("fa-battery-low");
-          element.classList.add("fa-bolt");
-          document.getElementById("bateryLevel").innerText = a; 
-        }
-                 
-   }
- 
-
-   battery.addEventListener('levelchange', function(){
-     updateLevelInfo();
-   });
-   function updateLevelInfo(){
-    var a = Math.round(battery.level * 100);
-      a =a + "%";
-    var element = document.getElementById("batstatus");
-
+	battery.addEventListener('levelchange', function(){
+    	updateLevelInfo();
+   	});
+   	function updateLevelInfo(){
+        var a = Math.round(battery.level * 100);
+        a =a + "%";
+	var element = document.getElementById("batstatus");
         if (battery.level == 1 ){
             document.getElementById("bateryLevel").innerText = a; 
             element.classList.add("fa-battery-full");
@@ -57,9 +52,7 @@ navigator.getBattery().then(function(battery) {
             element.classList.add("fa-battery-half");
             element.classList.remove("fa-battery-quarter");
             element.classList.remove("fa-bolt");
-            
         }
-    
         if (battery.level < .3 && battery.charging == false){
             document.getElementById("bateryLevel").innerText = a; 
             element.classList.remove("fa-battery-half");   
@@ -73,12 +66,11 @@ navigator.getBattery().then(function(battery) {
             element.classList.add("fa-battery-low");
             element.classList.remove("fa-bolt");
             alert("Batería Baja!, Conecta tu dispositivo");
-        }
-   }
+    	}
+   	}
  
-   
 //    battery.addEventListener('chargingtimechange', function(){
-//      updateChargingInfo();
+//    	updateChargingInfo();
 //    });
 //    function updateChargingInfo(){
 //      console.log("Tiempo de carga de la batería: "
@@ -92,5 +84,4 @@ navigator.getBattery().then(function(battery) {
 //      console.log("Tiempo de descarga de la batería: "
 //                   + battery.dischargingTime + " segundos");
 //    }
- });
- 
+});
